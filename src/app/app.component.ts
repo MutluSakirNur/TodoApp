8e1377636e0b6a73ad95e76324a35ead89f0138e
MutluSakir.Nur <MutluSakir.Nur@logo.com.tr>
2022-11-11 15:01:24 +0300

    demo commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,154 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Task } from './models/task';
+import { HttpClient } from '@angular/common/http';
+import { StringConsts } from './models/stringConsts';
+import { DialogService } from './dialog/dialog.service';
+import { CreateTaskParam } from './models/createTaskParam';
+import { TaskProcessResult } from './models/taskProcessesResult';
+
+const API_LIST_PENDING_URL = `${environment.apiUrl}/listPendingTasks`;
+const API_LIST_OVERDUE_URL = `${environment.apiUrl}/listOverdueTasks`;
+const API_POST_CREATE_TASK_URL = `${environment.apiUrl}/createTodoTask`;
+const API_POST_EDIT_TASK_URL = `${environment.apiUrl}/editTodoTask`;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'TodoList';
+  pendingTasks: Task[];
+  overdueTasks: Task[];
+  currentPendingPage = 1;
+  currentOverduePage = 1;
+  pendingTasksLength: number;
+  totalPage = 6;
+  overdueTasksLength: number;
+  noPendingTask: string;
+  noOverdueTask: string;
+  taskforEdit: Task;
+  taskforCreate: Task;
+  createTaskparam: CreateTaskParam;
+  ischecked: boolean = false;
+
+  constructor(private http: HttpClient, public dialogService: DialogService) {
+    this.initCreateTask();
+    this.initEditTask();
+    this.createTaskparam = new CreateTaskParam();
+  }
+
+  ngOnInit(): void {
+    this.noPendingTask = StringConsts.noPendingTasks;
+    this.noOverdueTask = StringConsts.noOverdueTasks;
+    this.getPendingTasks().subscribe((data) => {
+      this.pendingTasks = data;
+      this.pendingTasksLength = data.length;
+    });
+    this.getOverdueTasks().subscribe((data) => {
+      this.overdueTasks = data;
+      this.overdueTasksLength = data.length;
+    });
+  }
+
+  getPendingTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(API_LIST_PENDING_URL);
+  }
+
+  getOverdueTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(API_LIST_OVERDUE_URL);
+  }
+
+  pageChanged(id: string, event: number) {
+    if (id == 'pendingtasks') this.currentPendingPage = event;
+    else this.currentOverduePage = event;
+  }
+
+  pendingClicked() {
+    this.ischecked = false;
+    this.getPendingTasks().subscribe((data) => {
+      this.pendingTasks = data;
+      this.pendingTasksLength = data.length;
+    });
+  }
+
+  overdueClicked() {
+    this.ischecked = false;
+    this.getOverdueTasks().subscribe((data) => {
+      this.overdueTasks = data;
+      this.overdueTasksLength = data.length;
+    });
+  }
+
+  createTask() {
+    if (!(this.taskforCreate.title > '')) {
+      this.dialogService.openModal(
+        'Missing Field',
+        'Please type task title',
+        false
+      );
+      return;
+    } else {
+      this.createTaskparam.title = this.taskforCreate.title;
+      this.createTaskparam.dueDate = this.taskforCreate.dueDate;
+      this.createTaskProcesses().subscribe((data) => {
+        this.dialogService.openModal(
+          data.isSuccessful ? 'Success' : 'Error',
+          data.isSuccessful ? 'New task created' : data.errorMessage,
+          data.isSuccessful
+        );
+        if (data.isSuccessful) this.initCreateTask();
+      });
+    }
+  }
+
+  createTaskProcesses(): Observable<TaskProcessResult> {
+    return this.http.post<TaskProcessResult>(
+      API_POST_CREATE_TASK_URL,
+      this.createTaskparam
+    );
+  }
+
+  editTask() {
+    if (!(this.taskforEdit.title > '')) {
+      this.dialogService.openModal(
+        'Missing Field',
+        'Please type task title',
+        false
+      );
+      return;
+    } else {
+      this.editTaskProcesses().subscribe((data) => {
+        this.dialogService.openModal(
+          data.isSuccessful ? 'Success' : 'Error',
+          data.isSuccessful ? 'Task edited' : data.errorMessage,
+          data.isSuccessful
+        );
+        if (data.isSuccessful) this.initEditTask();
+      });
+    }
+  }
+
+  editTaskProcesses(): Observable<TaskProcessResult> {
+    return this.http.post<TaskProcessResult>(
+      API_POST_EDIT_TASK_URL,
+      this.taskforEdit
+    );
+  }
+
+  fillTaskforEdit(fillTask: Task) {
+    this.taskforEdit = fillTask;
+    this.ischecked = true;
+  }
+
+  initCreateTask() {
+    this.taskforCreate = new Task();
+    this.taskforCreate.dueDate = new Date();
+  }
+
+  initEditTask() {
+    this.taskforEdit = new Task();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,56 @@
+import { CommonModule, DatePipe } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgxPaginationModule } from 'ngx-pagination';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { AppComponent } from './app.component';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import {
+  MatNativeDateModule,
+  MAT_DATE_FORMATS,
+  MAT_DATE_LOCALE,
+} from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { DialogTemplateComponent } from './dialog-template/dialog-template.component';
+import { DialogService } from './dialog/dialog.service';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MY_FORMATS } from './models/dateformat';
+import {
+  MatDatetimepickerModule,
+  MatNativeDatetimeModule,
+} from '@mat-datetimepicker/core';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, DialogTemplateComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    NgxPaginationModule,
+    HttpClientModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    MatCheckboxModule,
+    MatDialogModule,
+    ReactiveFormsModule,
+    MatDatetimepickerModule,
+    MatNativeDatetimeModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    MatDatepickerModule,
+    DialogService,
+    { provide: MAT_DATE_FORMATS, useValue: MY_FORMATS },
+    DatePipe,
+    { provide: MAT_DATE_LOCALE, useValue: 'en-GB' },
+  ],
+  bootstrap: [AppComponent],
+  entryComponents: [DialogTemplateComponent],
 })
-export class AppModule { }
+export class AppModule {}
